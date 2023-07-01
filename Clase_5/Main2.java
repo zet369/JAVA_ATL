5ee53c675448bd138a4fd1162bc64a387dbf5f6b
Jonatan <jonatanroman999@gmail.com>
2023-06-30 22:24:01 -0500

    agregamos Main.java

--- Clase_5/Main2.java
+++ Clase_5/Main2.java
@@ -3,5 +3,6 @@ package Clase_5;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Seguimos aprendiendo");
+        System.out.println("Dit");
     }
 }
